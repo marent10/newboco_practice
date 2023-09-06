40f2c7ea75fa8f47b22d7369ebfb0ce53306c75a
Michael J. Arentson <michael.arentson@ccr.tv>
2023-09-06 10:54:15 -0500

    Adding my_module

---
+++ my_module.py
@@ -0,0 +1,17 @@
+#def test():
+#    print("hello")
+
+#my_score = 9001
+#print("the module was loaded.")
+
+def get_employees():
+    # do stuff
+    return[{"name": "Robbie", "money_get": 2}]
+
+def get_salary(employee):
+    return employee["money_get"]
+
+def give_raise(employee):
+    # do stuff
+    #modify employee with new salary
+    return employee
